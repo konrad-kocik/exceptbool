c21e955617da8b5682c44ca2534e26f2df965ee4
Konrad Kocik <konrad.kocik@gmail.com>
2019-01-12 19:42:03 +0100

    Fix except_converter UTs

--- exceptbool/except_converter.py
+++ exceptbool/except_converter.py
@@ -9,4 +9,4 @@ def except_converter(*, exc=Exception, to=False):
     try:
         yield result
     except exc:
-        result.wrap(bool(to))
+        result.value = to
--- tests/test_except_converter.py
+++ tests/test_except_converter.py
@@ -7,49 +7,49 @@ def test_except_converter_converts_to_wrapped_false_when_exception_raised():
     with except_converter() as converted_exception:
         raise Exception()
 
-    assert not converted_exception
+    assert not converted_exception.value
 
 
 def test_except_converter_converts_to_wrapped_true_when_exception_not_raised():
     with except_converter() as converted_exception:
         pass
 
-    assert converted_exception
+    assert converted_exception.value
 
 
 def test_except_converter_converts_to_wrapped_true_before_exception_raised():
     with except_converter() as converted_exception:
-        assert converted_exception
+        assert converted_exception.value
         raise Exception()
 
 
 def test_except_converter_changes_converted_value_to_wrapped_false_after_exception_raised():
     with except_converter() as converted_exception:
-        assert converted_exception
+        assert converted_exception.value
         raise Exception()
 
-    assert not converted_exception
+    assert not converted_exception.value
 
 
 def test_except_converter_converts_to_given_wrapped_bool_when_given_exception_raised():
     with except_converter(exc=ValueError, to=True) as converted_exception:
         raise ValueError()
 
-    assert converted_exception
+    assert converted_exception.value
 
 
 def test_except_converter_converts_to_given_wrapped_bool_negation_when_given_exception_not_raised():
     with except_converter(exc=TimeoutError, to=True) as converted_exception:
         pass
 
-    assert not converted_exception
+    assert not converted_exception.value
 
 
 def test_except_converter_converts_to_given_wrapped_bool_when_sub_exception_raised():
     with except_converter(exc=Exception, to=True) as converted_exception:
         raise TypeError()
 
-    assert converted_exception
+    assert converted_exception.value
 
 
 def test_except_converter_does_not_catch_exception_when_exception_different_than_given_exception_raised():
@@ -72,28 +72,28 @@ def test_except_converter_converts_to_wrapped_bool_representation_when_to_is_not
     with except_converter(to="") as converted_exception:
         raise TimeoutError
 
-    assert not converted_exception
+    assert not converted_exception.value
 
 
 def test_except_converter_converts_to_negated_wrapped_bool_representation_when_to_is_not_a_bool():
     with except_converter(to="") as converted_exception:
         pass
 
-    assert converted_exception
+    assert converted_exception.value
 
 
 def test_except_converter_converts_to_false_when_one_of_given_exceptions_raised():
     with except_converter(exc=(ConnectionError, KeyError)) as converted_exception:
         raise KeyError()
 
-    assert not converted_exception
+    assert not converted_exception.value
 
 
 def test_except_converter_converts_to_true_when_none_of_given_exceptions_raised():
     with except_converter(exc=(LookupError, FloatingPointError)) as converted_exception:
         pass
 
-    assert converted_exception
+    assert converted_exception.value
 
 
 def test_except_converter_does_not_catch_exception_when_exception_different_than_given_exceptions_raised():
