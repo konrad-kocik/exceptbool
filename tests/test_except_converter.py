706c83b399a6c813b14e5d22d6b347b9259700c8
Konrad Kocik <konrad.kocik@gmail.com>
2019-01-12 20:15:08 +0100

    Adjust except_converter UTs names and assertions

--- tests/test_except_converter.py
+++ tests/test_except_converter.py
@@ -3,53 +3,59 @@ from pytest import raises
 from exceptbool import except_converter
 
 
-def test_except_converter_converts_to_wrapped_false_when_exception_raised():
+def test_except_converter_converts_to_false_when_exception_raised():
     with except_converter() as converted_exception:
         raise Exception()
 
-    assert not converted_exception.value
+    assert converted_exception.value is False
 
 
-def test_except_converter_converts_to_wrapped_true_when_exception_not_raised():
+def test_except_converter_converts_to_true_when_exception_not_raised():
     with except_converter() as converted_exception:
         pass
 
-    assert converted_exception.value
+    assert converted_exception.value is True
 
 
-def test_except_converter_converts_to_wrapped_true_before_exception_raised():
+def test_except_converter_converts_to_true_before_exception_raised():
     with except_converter() as converted_exception:
-        assert converted_exception.value
+        assert converted_exception.value is True
         raise Exception()
 
 
-def test_except_converter_changes_converted_value_to_wrapped_false_after_exception_raised():
+def test_except_converter_changes_converted_value_to_false_after_exception_raised():
     with except_converter() as converted_exception:
-        assert converted_exception.value
+        assert converted_exception.value is True
         raise Exception()
 
-    assert not converted_exception.value
+    assert converted_exception.value is False
 
 
-def test_except_converter_converts_to_given_wrapped_bool_when_given_exception_raised():
-    with except_converter(exc=ValueError, to=True) as converted_exception:
+def test_except_converter_converts_to_given_bool_when_given_exception_raised():
+    expected_bool = True
+
+    with except_converter(exc=ValueError, to=expected_bool) as converted_exception:
         raise ValueError()
 
-    assert converted_exception.value
+    assert converted_exception.value is expected_bool
 
 
-def test_except_converter_converts_to_given_wrapped_bool_negation_when_given_exception_not_raised():
-    with except_converter(exc=TimeoutError, to=True) as converted_exception:
+def test_except_converter_converts_to_given_bool_negation_when_given_exception_not_raised():
+    expected_bool = False
+
+    with except_converter(exc=TimeoutError, to=not expected_bool) as converted_exception:
         pass
 
-    assert not converted_exception.value
+    assert converted_exception.value is expected_bool
 
 
-def test_except_converter_converts_to_given_wrapped_bool_when_sub_exception_raised():
-    with except_converter(exc=Exception, to=True) as converted_exception:
+def test_except_converter_converts_to_given_bool_when_sub_exception_raised():
+    expected_bool = True
+
+    with except_converter(exc=Exception, to=expected_bool) as converted_exception:
         raise TypeError()
 
-    assert converted_exception.value
+    assert converted_exception.value is expected_bool
 
 
 def test_except_converter_does_not_catch_exception_when_exception_different_than_given_exception_raised():
@@ -68,32 +74,32 @@ def test_except_converter_raises_exception_when_given_exception_is_not_an_except
     assert 'catching classes that do not inherit from BaseException is not allowed' in str(exc)
 
 
-def test_except_converter_converts_to_wrapped_bool_representation_when_to_is_not_a_bool():
+def test_except_converter_converts_to_bool_representation_when_to_is_not_a_bool():
     with except_converter(to="") as converted_exception:
         raise TimeoutError
 
-    assert not converted_exception.value
+    assert converted_exception.value is False
 
 
-def test_except_converter_converts_to_negated_wrapped_bool_representation_when_to_is_not_a_bool():
+def test_except_converter_converts_to_negated_bool_representation_when_to_is_not_a_bool():
     with except_converter(to="") as converted_exception:
         pass
 
-    assert converted_exception.value
+    assert converted_exception.value is True
 
 
 def test_except_converter_converts_to_false_when_one_of_given_exceptions_raised():
     with except_converter(exc=(ConnectionError, KeyError)) as converted_exception:
         raise KeyError()
 
-    assert not converted_exception.value
+    assert converted_exception.value is False
 
 
 def test_except_converter_converts_to_true_when_none_of_given_exceptions_raised():
     with except_converter(exc=(LookupError, FloatingPointError)) as converted_exception:
         pass
 
-    assert converted_exception.value
+    assert converted_exception.value is True
 
 
 def test_except_converter_does_not_catch_exception_when_exception_different_than_given_exceptions_raised():
